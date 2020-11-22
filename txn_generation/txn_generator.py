ab6cc446585c49eb8446b524b97eae6cd08a4e72
Sam Hastings <samhastingsis@gmail.com>
2020-11-22 16:48:45 +0000

    updated txn generator and made a million links

--- txn_generation/txn_generator.py
+++ txn_generation/txn_generator.py
@@ -5,17 +5,16 @@ import random
 from operator import add
 
 from pyspark.sql import SparkSession#, SQLContext
-from pyspark.sql.types import StructType, StructField, StringType, ArrayType
-from pyspark.sql.functions import udf, explode
+from pyspark.sql.types import StringType, ArrayType, IntegerType, DoubleType, StructType, StructField
+from pyspark.sql.functions import udf, explode, col
 import time
 
-num_steps = 2 #number of days in simulation
+num_steps = 365 #number of days in simulation
 
 
 def transact(txn_prob, txn_type, account_from, account_to, MoP, amount):
     random_numbers = [random.random() for x in range(num_steps)]
     return [[timestep, txn_type, account_from, account_to, MoP, amount] for timestep, x in enumerate(random_numbers) if x < txn_prob]
-
 
 if __name__ == "__main__":
 
@@ -34,7 +33,15 @@ if __name__ == "__main__":
     #print(type(df.select("txn_prob")))
     # convert transact() to a spark sql udf, explicitly setting its return type
     # to be a list of strings ArrayType(StringType())
-    udf_transact = udf(transact, ArrayType(StringType()))
+    udf_transact = udf(transact, ArrayType(ArrayType(StringType())))
+
+    '''StructType([
+    StructField("timestep", IntegerType(), nullable = True),
+    StructField("txn_type", StringType(), nullable = True),
+    StructField("account_from", StringType(), nullable = True),
+    StructField("account_to", StringType(), nullable = True),
+    StructField("MoP", StringType(), nullable = True),
+    StructField("amount", IntegerType(), nullable = True)]))))'''
     # add a new column "transactions" to df, the values of this column will be
     # the result of applying udf_transact elementwise to the txn_prob column.
     # N.B. inputs to sql udfs MUST BE columns, e.g. cannot be intergers
@@ -48,12 +55,26 @@ if __name__ == "__main__":
                                                    .alias("transactions"))
 
     list_length = len(transaction_explode_df.select('transactions').take(1)[0][0])
-    transaction_explode_df.show()
+    #transaction_explode_df.show()
+    '''Have a look at the first value in txn_eplxode_df, see what type it is  -
+    the error message looks like it might be a string. if it's a list see how you
+    refer to the elemtns of the list, if it's a string need to split on comma'''
+    #print(type(transaction_explode_df.head()[0]))
+    #print(transaction_explode_df.head()[0])
 
-    '''transaction_output_df = transaction_explode_df \
+    transaction_output_df = transaction_explode_df \
         .select([transaction_explode_df["transactions"][i] for i in range(list_length)])
 
-    transaction_output_df.show()'''
+    oldColumns = transaction_output_df.schema.names
+    newColumns = ["timestep", "txn_type", "account_from", "account_to", "MoP", "amount"]
+
+    names_as = [(oldColumns[idx], name) for idx, name in enumerate(newColumns)]
+
+    for name in names_as:
+        transaction_output_df = transaction_output_df.withColumnRenamed(name[0],name[1])
+
+    transaction_output_df.show()
+    print(transaction_output_df.count())
 
     print("Spark job takes {:.2f}s".format(time.time() - start))
 
